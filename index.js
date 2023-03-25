34532d2709d3f9f7b9004bbbbfbbbf889dd6dd2c
srdtrk <srdtrk@hotmail.com>
2023-03-25 19:41:52 +0300

    openai api added correctly, slack api is wrong

---
+++ index.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const { App } = require("@slack/bolt");
+const { WebClient } = require("@slack/web-api");
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const slackApp = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+});
+
+const webClient = new WebClient(process.env.SLACK_BOT_TOKEN);
+
+slackApp.event("app_mention", async ({ event, context }) => {
+  const prompt = `${event.text}`;
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: prompt }],
+  });
+
+  const reply = completion.data.choices[0].message;
+
+  try {
+    await webClient.chat.postMessage({
+      channel: event.channel,
+      text: reply,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+(async () => {
+  await slackApp.start();
+  console.log("Bot is running!");
+})();
